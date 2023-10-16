32f646226f29b33735e520841c0ae28a52361c8e
BSC <ben831137@gmail.com>
2023-10-17 00:41:48 +0800

    second  commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/ben831127.github.io/" : "/",
+  base: process.env.NODE_ENV === "production" ? "" : "/",
   plugins: [react()],
 });
